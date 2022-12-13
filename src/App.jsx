0f3a819179c6d317a646fa3cd577c115d8b03426
Fernando Ximenes <fxdias@gmail.com>
2022-12-13 11:36:05 -0300

    add scrolltotop and browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react"
+import { BrowserRouter, Routes, Routes, useLocation } from "react-router-dom"
+import Home from "./scenes/home/Home"
+
+// change BrowserRouter scroll behavior when navigating between page
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
 
 function App() {
 
   return (
     <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
