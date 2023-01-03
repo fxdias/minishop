e5329f46ef2781079a53d24b767a32010aa32874
Fernando Ximenes <fxdias@gmail.com>
2023-01-02 22:27:37 -0300

    add app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react"
 import { BrowserRouter, Routes, Routes, useLocation } from "react-router-dom"
 import Home from "./scenes/home/Home"
+import ItemDetails from "./scenes/itemDetails/ItemDetails"
+import Checkout from "./scenes/checkout/Checkout"
+import Confirmation from "./scenes/checkout/Confirmation"
 
 // change BrowserRouter scroll behavior when navigating between page
 const ScrollToTop = () => {
@@ -20,6 +23,9 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
