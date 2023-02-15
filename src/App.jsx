a81ac31614ce016d25e36b1202aef3a0f06769a1
Fernando Ximenes <fxdias@gmail.com>
2023-02-14 22:38:53 -0300

    add navbar component code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./scenes/home/Home"
 import ItemDetails from "./scenes/itemDetails/ItemDetails"
 import Checkout from "./scenes/checkout/Checkout"
 import Confirmation from "./scenes/checkout/Confirmation"
+import Navbar from "./scenes/global/Navbar"
 
 // change BrowserRouter scroll behavior when navigating between page
 const ScrollToTop = () => {
@@ -20,6 +21,7 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
